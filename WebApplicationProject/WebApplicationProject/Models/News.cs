387d4b8bd5a8f700d29ba902e60cc756235ae240
DarkReduX <furyredux@gmail.com>
2021-02-20 11:43:09 +0300

    add: DisplayAttributes for post model

--- WebApplicationProject/WebApplicationProject/Models/News.cs
+++ WebApplicationProject/WebApplicationProject/Models/News.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Microsoft.EntityFrameworkCore;
+﻿//using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 
 namespace WebApplicationProject.Models
 {
     public class News
     {
         public int ID { get; set; }
+        [Required]
+        [Display(Name = "Заголовок")]
         public string header { get; set; }
+        [Required]
+        [Display(Name = "Содержание поста")]
+        [DataType(DataType.MultilineText)]
         public string info { get; set; }
     }
 
