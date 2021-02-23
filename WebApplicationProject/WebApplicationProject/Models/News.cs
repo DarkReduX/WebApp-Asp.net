5679924628a0506d44863cc30cbfd2599a941bc7
DarkReduX <furyredux@gmail.com>
2021-02-23 12:06:02 +0300

    add: pageInfo class and NewsViewModel for pagination

--- WebApplicationProject/WebApplicationProject/Models/News.cs
+++ WebApplicationProject/WebApplicationProject/Models/News.cs
@@ -1,4 +1,7 @@
 ﻿//using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 
@@ -14,6 +17,21 @@ namespace WebApplicationProject.Models
         [Display(Name = "Содержание поста")]
         [DataType(DataType.MultilineText)]
         public string info { get; set; }
+    }
+    public class PageInfo
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems / PageSize); }
+        }
+    }
+    public class NewsViewModel
+    {
+        public List<News> posts { get; set; }
+        public PageInfo PageInfo { get; set; }
     }
 
 }
