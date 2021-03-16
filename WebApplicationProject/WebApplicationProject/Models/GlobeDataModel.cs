403104884bc5899641a28d0595bb83d228f05228
DarkReduX <furyredux@gmail.com>
2021-03-16 23:19:54 +0300

    add: globe data model

---
+++ WebApplicationProject/WebApplicationProject/Models/GlobeDataModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationProject.Models
+{
+    public class GlobeDataModel
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string header { get; set; }
+        public string countryName { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+
+ //"SELECT TOP 50 ID, header, latitude, longitude, countryName FROM [dbo].[News], [dbo].[Ips] WHERE ID = NewsId"
+    }
+}
