802f128450356b82f087b105a6623deb7abee126
DarkReduX <furyredux@gmail.com>
2021-03-17 20:21:13 +0300

    NewsContext inherit of IdentityDbContext, add Votes DbSet

--- WebApplicationProject/WebApplicationProject/Models/NewsContext.cs
+++ WebApplicationProject/WebApplicationProject/Models/NewsContext.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace WebApplicationProject.Models
 {
-    public class NewsContext : DbContext
+    public class NewsContext : IdentityDbContext
     {
         public NewsContext()
             : base("DefaultConnection")
         { }
         public DbSet<News> news { get; set; }
         public DbSet<Ip> Ips { get; set; }
+        public DbSet<Vote> Votes { get; set; }
     }
 }
